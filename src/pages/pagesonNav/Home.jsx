7376c67b15408d2813f3c6e0f07afb71307ad688
BinethmaJayawickrama <binethmad@gmail.com>
2026-02-25 10:35:50 +0530

    changes in homepage.jsx

--- src/pages/pagesonNav/Home.jsx
+++ src/pages/pagesonNav/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <div className="relative w-full overflow-hidden">
 
       {/* 2. HERO SECTION */}
-      <section className="relative pt-32 pb-20 px-6 max-w-5xl mx-auto z-10 text-center flex flex-col items-center gap-8">
+      <section className="relative pt-48 pb-20 px-6 max-w-5xl mx-auto z-10 text-center flex flex-col items-center gap-8">
 
         {/* Heading */}
         <h1 className="text-6xl md:text-8xl font-bold text-gray-900 leading-[1.1] tracking-tight">
